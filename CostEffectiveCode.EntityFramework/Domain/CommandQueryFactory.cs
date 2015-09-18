3f363051e0bd5d8f0f4b55480a3eed4ec6a0dcf6
Andrey Pesoshin <pesoshin@boommy.ru>
2015-09-18 13:17:58 +0300

    created CostEffectiveCode.EntityFramework assembly

---
+++ CostEffectiveCode.EntityFramework/Domain/CommandQueryFactory.cs
@@ -0,0 +1,83 @@
+﻿using System.Web.Mvc;
+using CostEffectiveCode.Domain.Cqrs.Commands;
+using CostEffectiveCode.Domain.Cqrs.Queries;
+using CostEffectiveCode.Domain.Ddd.Entities;
+using CostEffectiveCode.Domain.Ddd.Specifications;
+using CostEffectiveCode.Extensions;
+using JetBrains.Annotations;
+
+namespace CostEffectiveCode.EntityFramework.Domain
+{
+    [UsedImplicitly]
+    public class CommandQueryFactory : ICommandFactory, IQueryFactory
+    {
+        #region ICommandFactory Implementation
+
+        public TCommand GetCommand<TEntity, TCommand>() where TCommand : ICommand<TEntity>
+        {
+            return DependencyResolver.Current.GetService<TCommand>().CheckNotNull();
+        }
+
+        public T GetCommand<T>() where T:ICommand
+        {
+            return DependencyResolver.Current.GetService<T>().CheckNotNull();
+        }
+
+        public CreateEntityCommand<T> GetCreateCommand<T>() where T : class, IEntity
+        {
+            var createEntityCommand = DependencyResolver.Current.GetService<CreateEntityCommand<T>>();
+
+            return createEntityCommand.CheckNotNull();
+        }
+
+        public DeleteEntityCommand<T> GetDeleteCommand<T>() where T : class, IEntity
+        {
+            return DependencyResolver.Current.GetService<DeleteEntityCommand<T>>().CheckNotNull();
+        }
+
+        #endregion
+
+        #region IQueryFactory implementation
+
+        public IQuery<TEntity,IExpressionSpecification<TEntity>> GetQuery<TEntity>()
+            where TEntity : class, IEntity
+        {
+            return DependencyResolver.Current
+                .GetService<IQuery<TEntity, IExpressionSpecification<TEntity>>>()
+                .CheckNotNull();
+        }
+
+        public IQuery<TEntity, TSpecification> GetQuery<TEntity, TSpecification>()
+            where TEntity : class, IEntity
+            where TSpecification : ISpecification<TEntity>
+        {
+            return DependencyResolver.Current
+                .GetService<IQuery<TEntity, TSpecification>>()
+                .CheckNotNull();
+        }
+
+        public TQuery GetQuery<TEntity, TSpecification, TQuery>()
+            where TEntity : class, IEntity
+            where TSpecification : ISpecification<TEntity>
+            where TQuery: IQuery<TEntity, TSpecification>
+        {
+            return DependencyResolver.Current
+                .GetService<TQuery>()
+                .CheckNotNull();
+        }
+
+        public ISpecificQuery<TResult, TFilter> GetSpecificQuery<TQuery, TResult, TFilter>() where TQuery : ISpecificQuery<TResult, TFilter>
+        {
+            return DependencyResolver.Current.GetService<TQuery>();
+        }
+
+        public TQuery GetSpecificQuery<TQuery>()
+            where TQuery : ISpecificQuery
+        {
+            return DependencyResolver.Current.GetService<TQuery>().CheckNotNull();
+        }
+
+
+        #endregion
+    }
+}
---
+++ CostEffectiveCode.EntityFramework/Domain/ExpressionQueryFactory.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Web.Mvc;
+using CostEffectiveCode.Domain.Cqrs.Queries;
+using CostEffectiveCode.Domain.Ddd.Entities;
+using CostEffectiveCode.Domain.Ddd.Specifications;
+using CostEffectiveCode.Domain.Ddd.UnitOfWork;
+using CostEffectiveCode.Extensions;
+
+namespace CostEffectiveCode.EntityFramework.Domain
+{
+    public class ExpressionQueryFactory : IQueryFactory
+    {
+        private readonly ILinqProvider _linqProvider;
+
+        public ExpressionQueryFactory(ILinqProvider linqProvider)
+        {
+            _linqProvider = linqProvider;
+        }
+
+        public IQuery<TEntity, IExpressionSpecification<TEntity>> GetQuery<TEntity>()
+            where TEntity : class, IEntity
+        {
+            return new ExpressionQuery<TEntity>(_linqProvider);
+        }
+
+        public IQuery<TEntity, TSpecification> GetQuery<TEntity, TSpecification>() where TEntity : class, IEntity where TSpecification : ISpecification<TEntity>
+        {
+            throw new NotSupportedException();
+        }
+
+        public TQuery GetQuery<TEntity, TSpecification, TQuery>() where TEntity : class, IEntity where TSpecification : ISpecification<TEntity> where TQuery : IQuery<TEntity, TSpecification>
+        {
+            throw new NotSupportedException();
+        }
+
+        public TQuery GetSpecificQuery<TQuery>() where TQuery : ISpecificQuery
+        {
+            throw new NotSupportedException();
+        }
+    }
+}
