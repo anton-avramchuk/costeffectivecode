1d9acd9a6a62530ce90e198e538e6d058b8602dc
Andrey Pesoshin <pesoshin@gmail.com>
2015-09-27 15:35:15 +0300

    CEC-6 added publishing profile for coreclr console app

--- CostEffectiveCode.Console.Sample/Program.cs
+++ CostEffectiveCode.Console.Sample/Program.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
-namespace CostEffectiveCode.Console.Sample
+﻿namespace CostEffectiveCode.Console.Sample
 {
     public class Program
     {
