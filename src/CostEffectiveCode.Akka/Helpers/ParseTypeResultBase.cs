7c05c54f7aac9ed85036388cb7c841ef532a9ac1
Andrey Pesoshin <pesoshin@boommy.ru>
2015-09-29 16:49:07 +0300

    CEC-4 CostEffectiveCode.Akka library ported

---
+++ src/CostEffectiveCode.Akka/Helpers/ParseTypeResultBase.cs
@@ -0,0 +1,12 @@
+﻿namespace CostEffectiveCode.Akka.Helpers
+{
+    public abstract class ParseTypeResultBase
+    {
+        protected ParseTypeResultBase(bool isSubclass)
+        {
+            IsSubclass = isSubclass;
+        }
+
+        public bool IsSubclass { get; private set; }
+    }
+}
---
+++ src/CostEffectiveCode.Akka/Messages/UnsubscribeMessage.cs
@@ -0,0 +1,12 @@
+﻿using CostEffectiveCode.Domain.Cqrs.Commands;
+
+namespace CostEffectiveCode.Akka.Messages
+{
+    public class UnsubscribeMessage<T> : HasCommandMessageBase<T>
+    {
+        public UnsubscribeMessage(ICommand<T> command)
+            : base(command)
+        {
+        }
+    }
+}
