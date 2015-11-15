4b5b5bdb4365dae134c99b7f90473b20c4a019b8
Andrey Pesoshin <pesoshin@gmail.com>
2015-11-15 20:24:35 +0300

    regenerated asp.net 5 web sample project

---
+++ CostEffectiveCode.Backoffice.AspNet5.Web.Sample/Properties/AssemblyInfo.cs
@@ -0,0 +1,23 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("CostEffectiveCode.Backoffice.AspNet5.Web.Sample")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("HighTech LLC")]
+[assembly: AssemblyProduct("CostEffectiveCode.Backoffice.AspNet5.Web.Sample")]
+[assembly: AssemblyCopyright("Copyright ©  2015")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("ca388a95-ed59-43cc-9575-d8a30062fa59")]
