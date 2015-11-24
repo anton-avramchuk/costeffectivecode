08fd2b7fd29a0729790f37b6172fd74d3269ce37
Andrey Pesoshin <pesoshin@boommy.ru>
2015-11-24 14:11:27 +0300

    upgraded IDataContext

--- src/CostEffectiveCode/Domain/Ddd/UnitOfWork/IDataContext.cs
+++ src/CostEffectiveCode/Domain/Ddd/UnitOfWork/IDataContext.cs
@@ -1,9 +1,17 @@
-﻿using JetBrains.Annotations;
+﻿using System;
+using CostEffectiveCode.Domain.Ddd.Entities;
+using JetBrains.Annotations;
 
 namespace CostEffectiveCode.Domain.Ddd.UnitOfWork
 {
     [PublicAPI]
     public interface IDataContext : ILinqProvider, IUnitOfWork
     {
+        [CanBeNull]
+        T FindById<T, TPrimaryKey>(TPrimaryKey id)
+            where T : IEntityBase<TPrimaryKey>;
+
+        [CanBeNull]
+        object FindById(Type type, object id);
     }
 }
