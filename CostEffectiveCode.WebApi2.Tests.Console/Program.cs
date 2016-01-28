336cd31c83c89cd6999da41e4bdd66734fb9a1ef
Andrey Pesoshin <pesoshin@gmail.com>
2016-01-28 22:40:16 +0300

    restored console project

---
+++ CostEffectiveCode.WebApi2.Tests.Console/Program.cs
@@ -0,0 +1,29 @@
+﻿using System.Net.Http;
+using Microsoft.Owin.Hosting;
+
+namespace CostEffectiveCode.WebApi2.Tests.Console
+{
+    public class Program
+    {
+        private const string Uri = "http://localhost:7777/";
+
+        public static void Main(string[] args)
+        {
+            System.Console.WriteLine("Starting WebApi Self-Host server");
+
+            using (WebApp.Start<CostEffectiveCode.WebApi2.Tests.Startup>(Uri))
+            {
+                var client = new HttpClient();
+
+                var response = client.GetAsync($"{Uri}api/products").Result;
+
+                System.Console.WriteLine("\n" + response);
+                System.Console.WriteLine(response.Content.ReadAsStringAsync().Result);
+
+                System.Console.WriteLine("\n\nSelf-host server is still running. You can debug whatever you want!");
+                System.Console.ReadLine();
+            }
+
+        }
+    }
+}
