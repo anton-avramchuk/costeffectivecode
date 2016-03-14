d221690f8d5b8cd83dc8a62518867c9294c2912a
Andrey Pesoshin <pesoshin@gmail.com>
2016-03-14 13:52:56 +0300

    UL-526 added missing file

---
+++ src/CostEffectiveCode.Akka/Messages/ExecuteCommandMessage.cs
@@ -0,0 +1,21 @@
+﻿using JetBrains.Annotations;
+
+namespace CostEffectiveCode.Akka.Messages
+{
+    /// <summary>
+    /// Empty strongly-typed message to execute encapsulated command inside CommandActor
+    /// </summary>
+    public class ExecuteCommandMessage
+    {
+    }
+
+    public class ExecuteCommandMessage<TMessage>
+    {
+        public TMessage Message { get; private set; }
+
+        public ExecuteCommandMessage([CanBeNull] TMessage message)
+        {
+            Message = message;
+        }
+    }
+}
