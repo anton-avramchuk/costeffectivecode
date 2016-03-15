9821f2e7e3db73054e1e039ed7ad911879eb93c7
Andrey Pesoshin <pesoshin@gmail.com>
2016-03-16 01:11:53 +0300

    UL-526 restored ServiceBase (temporarily?)

---
+++ src/CostEffectiveCode/Domain/Ddd/ServiceBase.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using CostEffectiveCode.Common.Logger;
+using CostEffectiveCode.Common.Scope;
+using CostEffectiveCode.Domain.Cqrs.Commands;
+using CostEffectiveCode.Domain.Cqrs.Queries;
+using CostEffectiveCode.Domain.Ddd.UnitOfWork;
+
+namespace CostEffectiveCode.Domain.Ddd
+{
+    public abstract class ServiceBase
+    {
+        protected readonly ICommandFactory CommandFactory;
+        protected readonly IQueryFactory QueryFactory;
+        protected readonly ILogger Logger;
+        protected readonly IScope<IUnitOfWork> UowScope;
+
+        protected ServiceBase(IScope<IUnitOfWork> uowScope, ICommandFactory commandFactory, IQueryFactory queryFactory, ILogger logger)
+        {
+            if (uowScope == null)
+                throw new ArgumentNullException(nameof(uowScope));
+            if (commandFactory == null)
+                throw new ArgumentNullException(nameof(commandFactory));
+            if (queryFactory == null)
+                throw new ArgumentNullException(nameof(queryFactory));
+            this.CommandFactory = commandFactory;
+            this.QueryFactory = queryFactory;
+            this.Logger = logger;
+            this.UowScope = uowScope;
+        }
+    }
+}
