a44ea45eb0946d390d0abe2e78d2e393b27f5fb1
Andrey Pesoshin <pesoshin@hightech.today>
2016-09-05 04:29:26 +0300

    CostEffectiveCode.Automapper - alpha3 released

---
+++ src/CostEffectiveCode.AutoMapper/InstanceBasedAutoMapperWrapper.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Linq;
+using AutoMapper.QueryableExtensions;
+using AM = AutoMapper;
+using IMapper = CostEffectiveCode.Common.IMapper;
+
+namespace CostEffectiveCode.AutoMapper
+{
+    public class InstanceBasedAutoMapperWrapper : IMapper
+    {
+        public AM.IConfigurationProvider Configuration { get; private set; }
+        public AM.IMapper Instance { get; private set; }
+
+        public InstanceBasedAutoMapperWrapper(AM.IConfigurationProvider configuration, bool skipValidnessAssertation = false)
+        {
+            Configuration = configuration;
+
+            if (!skipValidnessAssertation)
+                configuration.AssertConfigurationIsValid();
+
+            Instance = configuration.CreateMapper();
+        }
+
+        public TReturn Map<TReturn>(object src) => Instance.Map<TReturn>(src);
+
+        public TReturn Map<TReturn>(object src, TReturn dest) => Instance.Map(src, dest);
+
+        public IQueryable<TReturn> Project<TSource, TReturn>(IQueryable<TSource> queryable)
+            => queryable.ProjectTo<TReturn>(Configuration);
+    }
+}
