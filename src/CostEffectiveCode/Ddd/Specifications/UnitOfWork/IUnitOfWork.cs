a0172bb96b0d155d83a477873cde7c836294b2c7
Andrey Pesoshin <pesoshin@hightech.today>
2016-09-24 01:35:06 +0300

    alpha5 - fixed bug

--- src/CostEffectiveCode/Ddd/Specifications/UnitOfWork/IUnitOfWork.cs
+++ src/CostEffectiveCode/Ddd/Specifications/UnitOfWork/IUnitOfWork.cs
@@ -13,7 +13,8 @@ namespace CostEffectiveCode.Ddd.Specifications.UnitOfWork
         void Delete<TEntity>(TEntity entity)
             where TEntity : class, IEntity;
 
-        TEntity Get<TEntity>(object id);
+        TEntity Get<TEntity>(object id)
+            where TEntity : class, IEntity;
 
         IEntity Get(Type entityType, object id);
 
